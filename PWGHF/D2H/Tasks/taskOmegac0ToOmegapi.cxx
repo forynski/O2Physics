forynski/O2Physics

commit a3e89c4e73594a73ef81951bf1158fbc14a9939b

[PWGHF] OmegaC task, enable running one data and one MC process function at t… (#11881)

PWGHF/D2H/Tasks/taskOmegac0ToOmegapi.cxx
@@ -102,9 +102,14 @@ struct HfTaskOmegac0ToOmegapi {
 
   void init(InitContext&)
   {
-    std::array<bool, 8> doprocess{doprocessDataWithKFParticle, doprocessMcWithKFParticle, doprocessDataWithKFParticleMl, doprocessMcWithKFParticleMl, doprocessDataWithKFParticleFT0C, doprocessDataWithKFParticleMlFT0C, doprocessDataWithKFParticleFT0M, doprocessDataWithKFParticleMlFT0M};
+    std::array<bool, 6> doprocess{doprocessDataWithKFParticle, doprocessDataWithKFParticleMl, doprocessDataWithKFParticleFT0C, doprocessDataWithKFParticleMlFT0C, doprocessDataWithKFParticleFT0M, doprocessDataWithKFParticleMlFT0M};
     if ((std::accumulate(doprocess.begin(), doprocess.end(), 0)) != 1) {
-      LOGP(fatal, "One and only one process function should be enabled at a time.");
+      LOGP(fatal, "One and only one data process function should be enabled at a time.");
+    }
+
+    std::array<bool, 2> doprocessMc{doprocessMcWithKFParticle, doprocessMcWithKFParticleMl};
+    if ((std::accumulate(doprocessMc.begin(), doprocessMc.end(), 0)) != 1) {
+      LOGP(fatal, "One and only one MC process function should be enabled at a time.");
     }
 
     const AxisSpec thnAxisMass{thnConfigAxisMass, "inv. mass (#Omega#pi) (GeV/#it{c}^{2})"};

commit 4c4f69cbe50c22d0ca7743bbdd804d32d7554ced

[PWGHF] taskOmegac0ToOmegapi: Fix bug introduced in #11881 (#11979)

PWGHF/D2H/Tasks/taskOmegac0ToOmegapi.cxx
@@ -121,13 +121,17 @@ struct HfTaskOmegac0ToOmegapi {
   void init(InitContext&)
   {
     std::array<bool, 6> doprocess{doprocessDataWithKFParticle, doprocessDataWithKFParticleMl, doprocessDataWithKFParticleFT0C, doprocessDataWithKFParticleMlFT0C, doprocessDataWithKFParticleFT0M, doprocessDataWithKFParticleMlFT0M};
-    if ((std::accumulate(doprocess.begin(), doprocess.end(), 0)) != 1) {
-      LOGP(fatal, "One and only one data process function should be enabled at a time.");
+    if (std::accumulate(doprocess.begin(), doprocess.end(), 0) > 1) {
+      LOGP(fatal, "At most one data process function should be enabled at a time.");
     }
 
     std::array<bool, 2> doprocessMc{doprocessMcWithKFParticle, doprocessMcWithKFParticleMl};
-    if ((std::accumulate(doprocessMc.begin(), doprocessMc.end(), 0)) != 1) {
-      LOGP(fatal, "One and only one MC process function should be enabled at a time.");
+    if (std::accumulate(doprocessMc.begin(), doprocessMc.end(), 0) > 1) {
+      LOGP(fatal, "At most one MC process function should be enabled at a time.");
+    }
+
+    if ((std::accumulate(doprocess.begin(), doprocess.end(), 0) + std::accumulate(doprocessMc.begin(), doprocessMc.end(), 0)) == 0) {
+      LOGP(fatal, "At least one process function should be enabled.");
     }
 
     const AxisSpec thnAxisMass{thnConfigAxisMass, "inv. mass (#Omega#pi) (GeV/#it{c}^{2})"};